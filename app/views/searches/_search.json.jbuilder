27f72e83fec7fe21a0253d88b520d570a5c29aee
philipbroadway <me@philipbroadway.com>
2023-11-28 05:44:19 -0700

    init light n tight fe for api

---
+++ app/views/searches/_search.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! search, :id, :address, :data, :error, :created_at, :updated_at
+json.url search_url(search, format: :json)
---
+++ db/migrate/0_create_searches.rb
@@ -0,0 +1,11 @@
+class CreateSearches < ActiveRecord::Migration[7.1]
+  def change
+    create_table :searches do |t|
+      t.string :address, null: false
+      t.json :data, null: true
+      t.json :error, null: true
+
+      t.timestamps
+    end
+  end
+end
